f58107e84f22286a897a152d20a3233e91e7297f
goncaloach <goncaloach@gmail.com>
2022-02-22 21:58:53 +0000

    gugasv2

--- teste/src/Hello.java
+++ teste/src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Commit 21:56");
+        System.out.println();
     }
 }
